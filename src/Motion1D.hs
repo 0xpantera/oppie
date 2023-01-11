11ca02274c1498c141f2d28079b69375f3c786e9
0xpantera <0xpantera@proton.me>
2023-01-11 15:55:03 -0600

    Define derivative type synonym and function

--- src/Motion1D.hs
+++ src/Motion1D.hs
@@ -9,3 +9,8 @@ type Velocity = R
 
 type PositionFunc = Time -> Position
 type VelocityFunc = Time -> Velocity
+
+type Derivative = (R -> R) -> R -> R
+
+derivative :: R -> Derivative
+derivative dt x t = (x (t + dt/2) - x(t - dt/2)) / dt
