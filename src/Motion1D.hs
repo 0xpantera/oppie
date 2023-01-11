bc5af9a31148d2a64747d9d5128c7d27760b2146
0xpantera <0xpantera@proton.me>
2023-01-11 17:18:07 -0600

    Model acceleration

--- src/Motion1D.hs
+++ src/Motion1D.hs
@@ -6,6 +6,7 @@ type Time = R
 type TimeInterval = R
 type Position = R
 type Velocity = R
+type Acceleration = R
 
 type PositionFunc = Time -> Position
 type VelocityFunc = Time -> Velocity
@@ -29,3 +30,13 @@ velFromPos dt pos = derivative dt pos
 positionCV :: Position -> Velocity -> Time -> Position
 positionCV x0 v0 t = x0 + v0 * t
 
+accFromVel :: R                      -- dt
+           -> (Time -> Velocity)     -- velocity function
+           -> (Time -> Acceleration) -- acceleration function
+accFromVel = derivative
+
+velocityCA :: Velocity -> Acceleration -> Time -> Velocity
+velocityCA v0 a0 t = v0 + a0 * t
+
+positionCA :: Position -> Velocity -> Acceleration -> Time -> Position
+positionCA x0 v0 a0 t = a0 * t**2 / 2 + v0 * t + x0
