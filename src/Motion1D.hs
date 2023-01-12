0bb41a55463f0834eec634bdf736838e5a66267d
0xpantera <0xpantera@proton.me>
2023-01-12 15:41:34 -0600

    Implement antiderivatives

--- src/Motion1D.hs
+++ src/Motion1D.hs
@@ -16,6 +16,21 @@ type Derivative = (R -> R) -> R -> R
 derivative :: R -> Derivative
 derivative dt pos t = (pos (t + dt/2) - pos (t - dt/2)) / dt
 
+type Integration = (R -> R) -- function
+                -> R        -- lower limit
+                -> R        -- upper limit
+                -> R        -- result
+
+integral :: R -> Integration
+integral dt f a b = sum [f t * dt | t <- [a+dt/2, a+3*dt/2 .. b - dt/2]]
+
+type AntiDerivative =  R       -- initial value
+                   -> (R -> R) -- function
+                   -> (R -> R) -- antiderivative of function
+
+antiDerivative :: R -> AntiDerivative
+antiDerivative dt v0 a t = v0 + integral dt a 0 t            
+
 carPosition :: Time -> Position
 carPosition t = cos t
 
@@ -40,3 +55,20 @@ velocityCA v0 a0 t = v0 + a0 * t
 
 positionCA :: Position -> Velocity -> Acceleration -> Time -> Position
 positionCA x0 v0 a0 t = a0 * t**2 / 2 + v0 * t + x0
+
+velFromAcc :: R                      -- dt
+           -> Velocity               -- initial velocity
+           -> (Time -> Acceleration) -- acceleration function
+           -> (Time -> Velocity)     -- velocity function
+velFromAcc dt v0 a t = antiDerivative dt v0 a t
+
+posFromVel :: R                  -- dt
+           -> Position           -- initial position
+           -> (Time -> Velocity) -- velocity function
+           -> (Time -> Position) -- position function
+posFromVel = antiDerivative
+
+integralN' :: Int -> Integration
+integralN' n f a b =
+    integral dt f a b
+    where dt = (b - a) / fromIntegral n
