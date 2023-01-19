3c7ae9488c5877748681bf6ea91b95555d0438e6
0xpantera <0xpantera@proton.me>
2023-01-19 12:52:57 -0600

    Plot with custom label at peak projectile height

--- app/Main.hs
+++ app/Main.hs
@@ -14,14 +14,29 @@ yPos :: R -- y0
 yPos y0 vy0 ay t = y0 + vy0 * t + ay * t**2 / 2
 
 plot1 :: IO ()
-plot1 = plotFunc [Title "Projectile Motion"
-                 ,XLabel "Time (s)"
-                 ,YLabel "Height of projectile (m)"
-                 ,PNG "projectile.png"
-                 ,Key Nothing
+plot1 = plotFunc [ Title "Projectile Motion"
+                 , XLabel "Time (s)"
+                 , YLabel "Height of projectile (m)"
+                 , PNG "projectile.png"
+                 , Key Nothing
                  ] tRange (yPos 0 20 (-9.8))
+
+customLabel :: (R, R) -> String -> Attribute
+customLabel (x, y) label = 
+  Custom "label" ["\"" ++ label ++ "\"" ++ " at "
+                  ++ show x ++ "," ++ show y]
+
+plotCustom :: IO ()
+plotCustom =
+  plotFunc [ Title "Projectile Motion"
+           , XLabel "Time (s)"
+           , YLabel "Height of projectile (m)"
+           , customLabel (1.5, 22) "Peak Height"
+           , PNG "projectileLabel.png"
+           , Key Nothing
+           ] tRange (yPos 0 20 (-9.8))                              
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  plot1
+  plotCustom
