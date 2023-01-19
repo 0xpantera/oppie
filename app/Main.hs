9ca85f73c9ccc7e8db5f267f5c0263789cf1c1d3
0xpantera <0xpantera@proton.me>
2023-01-19 12:44:14 -0600

    Test simple plot

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,27 @@
 module Main where
+import Graphics.Gnuplot.Simple
 
-import qualified MyLib (someFunc)
+type R = Double
+
+tRange :: [R]
+tRange = [0, 0.01 .. 5]
+
+yPos :: R -- y0
+     -> R -- vy0
+     -> R -- ay
+     -> R -- t
+     -> R --y
+yPos y0 vy0 ay t = y0 + vy0 * t + ay * t**2 / 2
+
+plot1 :: IO ()
+plot1 = plotFunc [Title "Projectile Motion"
+                 ,XLabel "Time (s)"
+                 ,YLabel "Height of projectile (m)"
+                 ,PNG "projectile.png"
+                 ,Key Nothing
+                 ] tRange (yPos 0 20 (-9.8))
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  plot1
